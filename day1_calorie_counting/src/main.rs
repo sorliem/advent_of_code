efc81a76a120fc12f5f2858f6df824d029a1c0ec
Miles Sorlie <msorlie@protonmail.com>
2022-12-02 15:34:11 -0700

    add gitignore

--- day1_calorie_counting/src/main.rs
+++ day1_calorie_counting/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     let split = include_str!("../input").split("\n");
     let lines = split.collect::<Vec<&str>>();
     let (mut cur_elf_calories, mut biggest_elf_calories) = (0, 0);
+    let mut totals = vec![];
 
     for s in lines {
         if s == "" {
@@ -9,12 +10,22 @@ fn main() {
                 println!("NEW MAX!: {} -> {}", biggest_elf_calories, cur_elf_calories);
                 biggest_elf_calories = cur_elf_calories;
             }
+            totals.push(cur_elf_calories);
             cur_elf_calories = 0;
         } else {
             let calories = s.parse::<usize>().unwrap();
             cur_elf_calories += calories;
         }
     }
+    totals.sort();
+
+    let sum_top_three: usize = totals.clone().into_iter().rev().take(3).sum();
 
     println!("biggest calorie amount from single elf: {}", biggest_elf_calories);
+    println!("sum of top 3 calorie counts: {:?}", sum_top_three);
 }
+
+fn foo(totals: usize) {
+    println!("totals: {:?}", totals)
+}
+
