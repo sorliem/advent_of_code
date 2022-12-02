b2372ea93f19042dcfac53ad3fcb6b8c58e28d65
Miles Sorlie <msorlie@protonmail.com>
2022-12-02 15:40:04 -0700

    untrack rust target files

--- day1_calorie_counting/target/debug/deps/day1_calorie_counting-92de31a448d6ddfd.d
+++
@@ -1,6 +0,0 @@
-/home/miles/gitroot/src/aoc_2022/day1_calorie_counting/target/debug/deps/day1_calorie_counting-92de31a448d6ddfd.rmeta: src/main.rs src/../input
-
-/home/miles/gitroot/src/aoc_2022/day1_calorie_counting/target/debug/deps/day1_calorie_counting-92de31a448d6ddfd.d: src/main.rs src/../input
-
-src/main.rs:
-src/../input:
--- day1_calorie_counting/target/debug/deps/day1_calorie_counting-b3c1b91968713bde.d
+++
@@ -1,6 +0,0 @@
-/home/miles/gitroot/src/aoc_2022/day1_calorie_counting/target/debug/deps/day1_calorie_counting-b3c1b91968713bde.rmeta: src/main.rs src/../input
-
-/home/miles/gitroot/src/aoc_2022/day1_calorie_counting/target/debug/deps/day1_calorie_counting-b3c1b91968713bde.d: src/main.rs src/../input
-
-src/main.rs:
-src/../input:
