068eef5df77a72fa5ca1586bb4278a4498149c34
Miles Sorlie <msorlie@protonmail.com>
2022-12-04 07:56:33 -0700

    add day3 partial rust

--- day3_rucksack/src/main.rs
+++ day3_rucksack/src/main.rs
@@ -1,3 +1,18 @@
+use std::fmt::Debug;
+
+#[derive(Debug)]
+struct Rucksack<'a> {
+    compartment1: &'a str,
+    compartment2: &'a str,
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let data = include_str!("../test_input").split("\n").collect::<Vec<&str>>();
+
+    for d in &data {
+        println!("d: {}", d);
+    }
+
+    println!("data: {:?}", data);
 }
