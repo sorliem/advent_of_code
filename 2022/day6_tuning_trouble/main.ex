58b7bf6325c921f92be5a30595aaee8bcebd40c4
Miles Sorlie <msorlie@protonmail.com>
2022-12-06 19:36:16 -0700

    day6 double star complete

---
+++ 2022/day6_tuning_trouble/main.ex
@@ -0,0 +1,60 @@
+defmodule Day6TuningTrouble do
+  @moduledoc """
+  """
+
+  def run(file) do
+    lines =
+      file
+      |> File.read!()
+      |> String.split("\n", trim: true)
+
+    Enum.each(lines, &pt1_find_start_of_packet/1)
+    Enum.each(lines, &pt2_find_start_of_message/1)
+  end
+
+  defp pt1_find_start_of_packet(buffer) do
+    len = String.length(buffer)
+
+    Enum.reduce_while(0..len, nil, fn i, _acc ->
+      quad = String.slice(buffer, i, 14)
+
+
+      unique? =
+        quad
+        |> String.codepoints()
+        |> Enum.uniq()
+        |> (fn l -> length(l) == 4 end).()
+
+        if unique? do
+          pos = i + 4
+          IO.inspect({quad, pos}, label: "pt1. unique start-of-packet marker")
+          {:halt, nil}
+        else
+          {:cont, nil}
+        end
+    end)
+  end
+
+  defp pt2_find_start_of_message(buffer) do
+    len = String.length(buffer)
+
+    Enum.reduce_while(0..len, nil, fn i, _acc ->
+      slice = String.slice(buffer, 0+i, 14)
+
+      unique? =
+        slice
+        |> String.codepoints()
+        |> Enum.uniq()
+        |> (fn l -> length(l) == 14 end).()
+
+        if unique? do
+          IO.inspect({slice, i + 14}, label: "pt2. unique start-of-message marker")
+          {:halt, nil}
+        else
+          {:cont, nil}
+        end
+    end)
+  end
+end
+
+Day6TuningTrouble.run("input")
