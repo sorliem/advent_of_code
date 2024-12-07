e6d41a8b9955688242b68c934b18a29caf05c19e
Miles Sorlie <msorlie@protonmail.com>
2024-12-07 12:22:46 -0700

    day 7 part 2 done

--- 2024/day7_bridge_repair/main.exs
+++ 2024/day7_bridge_repair/main.exs
@@ -5,8 +5,7 @@ defmodule Day7 do
 
   def part1(equations) do 
     equations
-    # |> Enum.take(2)
-    |> Enum.map(fn {total, vals} ->
+    |> Stream.map(fn {total, vals} ->
       ops_for_len = gen_ops(length(vals))
       [hd | tail] = vals
       
@@ -17,12 +16,12 @@ defmodule Day7 do
             Enum.zip(oplist, tail)
             |> Enum.flat_map(fn {op, val} -> [op, val] end)
 
-          full_eq = [hd | inter]
+          [hd | inter]
         end)
 
       {total, equations}
     end)
-    |> Enum.flat_map(fn {total, equations} ->
+    |> Stream.flat_map(fn {total, equations} ->
       if Enum.any?(equations, fn equation -> sum(equation) == total end) do
         [total]
       else
@@ -39,8 +38,10 @@ defmodule Day7 do
         int, {_acc, nil} when is_integer(int) -> {int, nil}
         "+", {acc, nil} -> {acc, "+"}
         "*", {acc, nil} -> {acc, "*"}
+        "|", {acc, nil} -> {acc, "|"}
         int, {acc, "+"} -> {acc + int, nil} 
         int, {acc, "*"} -> {acc * int, nil} 
+        int, {acc, "|"} -> {String.to_integer("#{acc}#{int}"), nil} 
       end)
 
     sum
@@ -62,7 +63,68 @@ defmodule Day7 do
     base_a = base <> "*"
     base_b = base <> "+"
     new_bases = [base_a | [base_b | bases]]
-    gen_op(base_a, n-1, new_bases) ++ gen_op(base_b, n-1, new_bases)
+
+    [gen_op(base_a, n-1, new_bases) | [gen_op(base_b, n-1, new_bases)]]
+  end
+
+  def gen_op_map(n \\ 12) do
+    2..n
+    |> Enum.into(%{}, fn i -> 
+      {i, gen_ops2(i)}
+    end)
+  end
+
+  def part2(equations) do
+    op_map = gen_op_map()
+
+    equations
+    # |> Enum.take(2)
+    |> Enum.map(fn {total, vals} ->
+      ops_for_len = Map.get(op_map, length(vals))
+
+      [hd | tail] = vals
+      
+      equations = 
+        Enum.map(ops_for_len, fn opstr ->
+          oplist = String.split(opstr, "", trim: true)
+          inter = 
+            Enum.zip(oplist, tail)
+            |> Enum.flat_map(fn {op, val} -> [op, val] end)
+
+            [hd | inter]
+        end)
+
+      {total, equations}
+    end)
+    |> Enum.flat_map(fn {total, equations} ->
+      if Enum.any?(equations, fn equation -> sum(equation) == total end) do
+        [total]
+      else
+        []
+      end
+    end)
+    |> Enum.sum()
+    |> IO.inspect(label: "part2 answer")
+  end
+
+  def gen_ops2(n) do
+    start_ops = ["*", "+", "|"]
+
+    Enum.map(start_ops, fn base ->
+      gen_op2(base, n-1, [])
+    end)
+    |> List.flatten()
+    |> Enum.filter(fn op -> String.length(op) == n end) |> Enum.uniq()
+  end
+  
+  defp gen_op2(_base, 0, bases), do: bases
+  defp gen_op2(base, n, bases) do
+    base_a = base <> "*"
+    base_b = base <> "+"
+    base_c = base <> "|"
+    new_bases = [base_a | [base_b | [base_c | [ bases ]]]]
+    
+    [gen_op2(base_a, n-1, new_bases) | [ gen_op2(base_b, n-1, new_bases) | [gen_op2(base_c, n-1, new_bases)]]]
   end
 end
 
@@ -85,4 +147,5 @@ equations =
   # |> IO.inspect(label: "equations")
 
 Day7.part1(equations)
+Day7.part2(equations)
   
